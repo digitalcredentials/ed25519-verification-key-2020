1c676164dcebcefbbd6dc1c5aa3bbd45e8f851bf
Dave Longley <dlongley@digitalbazaar.com>
2022-05-05 19:15:35 -0400

    Update changelog.

--- karma.conf.js
+++ karma.conf.js
@@ -28,10 +28,7 @@ module.exports = function(config) {
     webpack: {
       //mode: 'production',
       mode: 'development',
-      devtool: 'inline-source-map',
-      node: {
-        crypto: false
-      }
+      devtool: 'inline-source-map'
     },
 
     // test results reporter to use
